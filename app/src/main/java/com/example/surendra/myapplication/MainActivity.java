de083eee49710453fbbde52e2068a857fd45b349
surendraparmar1998 <parmarsurendra415@gmail.com>
2019-05-07 10:25:07 +0530

    commit 1

--- app/src/main/java/com/example/surendra/myapplication/MainActivity.java
+++ app/src/main/java/com/example/surendra/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.surendra.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("TAG","Onstart");
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("TAG","OnResume");
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("TAG","OnDestroy");
+    }
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("TAG","OnStop");
+    }
+
 }
